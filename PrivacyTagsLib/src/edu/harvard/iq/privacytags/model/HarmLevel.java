98de07030de37e1bdc7d73ba1e524bf8997f387e
Michael Bar-Sinai <mich.barsinai@gmail.com>
2013-08-22 11:41:41 -0400

    Added Harvard's level to the harm level class.

--- PrivacyTagsLib/src/edu/harvard/iq/privacytags/model/HarmLevel.java
+++ PrivacyTagsLib/src/edu/harvard/iq/privacytags/model/HarmLevel.java
@@ -6,20 +6,32 @@ package edu.harvard.iq.privacytags.model;
  * @author michael
  */
 public enum HarmLevel {
-	NoRisk( new PrivacyTagSet(ApprovalType.None, AuthenticationType.None, DataUseAgreement.None, EncryptionType.Clear, EncryptionType.Clear) ),
-	Forsaken( new PrivacyTagSet(ApprovalType.None, AuthenticationType.Email_or_OAuth, DataUseAgreement.None, EncryptionType.Clear, EncryptionType.Clear) ),
-	Shamed( new PrivacyTagSet(ApprovalType.Online, AuthenticationType.Password, DataUseAgreement.Required, EncryptionType.Encrypted, EncryptionType.Encrypted) ),
-	CivilPenalties( new PrivacyTagSet(ApprovalType.Signed, AuthenticationType.Password, DataUseAgreement.Required, EncryptionType.Encrypted, EncryptionType.Encrypted) ),
-	CriminalPenalties( new PrivacyTagSet(ApprovalType.Signed, AuthenticationType.TwoFactor, DataUseAgreement.Required, EncryptionType.Encrypted, EncryptionType.Encrypted) ),
-	MaxControl( new PrivacyTagSet(ApprovalType.Signed, AuthenticationType.TwoFactor, DataUseAgreement.Required, EncryptionType.DoubleEncryption, EncryptionType.DoubleEncryption) );
+	NoRisk( 
+		1,   new PrivacyTagSet(ApprovalType.None,   AuthenticationType.None,           DataUseAgreement.None,      EncryptionType.Clear,     EncryptionType.Clear) ),
+	Forsaken( 
+		2,   new PrivacyTagSet(ApprovalType.None,   AuthenticationType.Email_or_OAuth, DataUseAgreement.None,      EncryptionType.Clear,     EncryptionType.Clear) ),
+	Shamed( 
+		3,   new PrivacyTagSet(ApprovalType.Online, AuthenticationType.Password,       DataUseAgreement.Required,  EncryptionType.Encrypted, EncryptionType.Encrypted) ),
+	CivilPenalties( 
+		4,   new PrivacyTagSet(ApprovalType.Signed, AuthenticationType.Password,       DataUseAgreement.Required,  EncryptionType.Encrypted, EncryptionType.Encrypted) ),
+	CriminalPenalties(
+		4,   new PrivacyTagSet(ApprovalType.Signed, AuthenticationType.TwoFactor,      DataUseAgreement.Required,  EncryptionType.Encrypted, EncryptionType.Encrypted) ),
+	MaxControl( 
+		4.5, new PrivacyTagSet(ApprovalType.Signed, AuthenticationType.TwoFactor,      DataUseAgreement.Required,  EncryptionType.DoubleEncryption, EncryptionType.DoubleEncryption) );
 	
 	private final PrivacyTagSet tags; 
-
-	HarmLevel( PrivacyTagSet aTagSet ) {
+	private final double harvardSecurityLevel;
+	
+	HarmLevel( double aSecurityLevel, PrivacyTagSet aTagSet ) {
+		harvardSecurityLevel = aSecurityLevel;
 		tags = aTagSet;
 	}
 	
 	public PrivacyTagSet tags() {
 		return tags;
+	}
+	
+	public double harvardSecurityLevel() {
+		return harvardSecurityLevel;
 	}
 }
