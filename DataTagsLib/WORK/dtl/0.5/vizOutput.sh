b507541ddf731ddbc6b3b384120f7406a60b128e
Michael Bar-Sinai <mich.barsinai@gmail.com>
2014-07-01 12:04:18 -0400

    Updated the interview to support the new tags. Added screening question

--- DataTagsLib/WORK/dtl/0.5/vizOutput.sh
+++ DataTagsLib/WORK/dtl/0.5/vizOutput.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-
-circo -Tpdf definitions.dtl.gv > out-definitions.pdf
+dot -Tpdf definitions.dtl.gv > out-definitions.pdf
 dot -Tpdf Questionnaire.dtf-ast.gv > out-ast.pdf
 dot -Tpdf Questionnaire.dtf-fcs.gv > out-flowchart.pdf
