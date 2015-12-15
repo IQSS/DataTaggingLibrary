8eab8d3aba28e7af69fb1721235a4e817399e4f3
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-12-14 19:44:04 -0500

    New path-based tagspace visualizer

---
+++ DataTagsLib/src/edu/harvard/iq/datatags/mains/Visualizers.java
@@ -0,0 +1,35 @@
+package edu.harvard.iq.datatags.mains;
+
+import edu.harvard.iq.datatags.model.types.CompoundType;
+import edu.harvard.iq.datatags.parser.exceptions.SemanticsErrorException;
+import edu.harvard.iq.datatags.parser.exceptions.SyntaxErrorException;
+import edu.harvard.iq.datatags.parser.tagspace.TagSpaceParser;
+import edu.harvard.iq.datatags.visualizers.graphviz.GraphvizTagSpacePathsVizualizer;
+import edu.harvard.iq.datatags.visualizers.graphviz.GraphvizVisualizer;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Run visualizers (mainly for testing them).
+ * @author michael
+ */
+public class Visualizers {
+    
+    
+    public static void main(String[] args) throws IOException, SyntaxErrorException, SemanticsErrorException {
+        Path tagsFile = Paths.get(args[0]);
+        
+        System.out.println("Reading tags: " + tagsFile );
+        System.out.println(" (full:  " + tagsFile.toAbsolutePath() + ")" );
+        
+        TagSpaceParser tagsParser = new TagSpaceParser();
+        CompoundType baseType = tagsParser.parse(tagsFile).buildType("DataTags").get();
+        
+        GraphvizVisualizer tagViz = new GraphvizTagSpacePathsVizualizer(baseType);
+        Path tagsOutPath = tagsFile.resolveSibling(tagsFile.getFileName().toString() + ".gv");
+        System.out.println("Writing " + tagsOutPath );
+		tagViz.vizualize( tagsOutPath );
+    }
+    
+}
